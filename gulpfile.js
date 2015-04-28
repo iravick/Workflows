e1356da84e0a2aee7bff7463dae454ea01a24a49
Ira Vick <ira@basecampwebdesign.com>
2015-04-28 17:52:13 -0400

    Added Browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	coffee = require('gulp-coffee');
+	browserify = require('gulp-browserify');
 	concat = require('gulp-concat');
+
+
 
 var coffeeSources =['components/coffee/tagline.coffee'];
 var jsSources = [
@@ -21,5 +24,6 @@ gulp.task('coffee', function() {
 gulp.task('js', function() {
 	gulp.src(jsSources)
 		.pipe(concat('script.js'))
+		.pipe(browserify())
 		.pipe(gulp.dest('builds/development/js'))
 });
